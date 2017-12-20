1bc002903e5e59132fdb3b6233bfd4080bb8b966
Micael Levi <mllc@icomp.ufam.edu.br>
2017-12-19 22:08:01 -0400

    fixed issues and set colors

--- done.sh
+++ done.sh
@@ -9,23 +9,42 @@
 ##  e/ou só atualizar o contador (badge) de tarefas prontas.
 ##
 ##  using:
-##  echo, grep, sed, mapfile, awk, read
+##  echo, grep, cut, sed, mapfile, awk, read
 ##
 
 
 FILE="README.md"
+MISCELLANEOUS="avulsos"
 CURR_DIR="${1%%/}"
-PATH_TO_FILE="$CURR_DIR/$FILE"
+PATH_TO_FILE="${CURR_DIR,,}/$FILE"
+MISCELLANEOUS_DIR="${CURR_DIR,,}/$MISCELLANEOUS"
 
-test $# -ne 1 && {
-  echo -e "usage: \e[36m$0\e[0m \e[35;1m<lang_directory>\e[0m"
-  exit 1
-}
-test -w "$PATH_TO_FILE" || exit 2 ## TODO: indiciar que o arquivo não existe para escrita
 
+[[ $# -ne 1 ]] && { echo -e "usage: \e[36m$0\e[0m \e[35;1m<lang_directory>\e[0m"; exit 1; }
+[[ -w "$PATH_TO_FILE" ]] || exit 2 ## TODO: indiciar que o arquivo não existe para escrita
+
+
+declare -A COLORS=( [w]=$'\e[37;1m' [y]=$'\e[33;1m' [g]=$'\e[32;1m' [r]=$'\e[31;1m' [p]=$'\e[35;1m' [n]=$'\e[0m' )
 declare -a tasks_not_done
 declare -i nums_tasks nums_tasks_done num_task line_selected_task
-declare -x percentage list_not_done task_name
+declare -x percentage list_not_done task_name normalized_task_name file dir task_ref
+declare -f set_file_and_dir
+
+
+function set_file_and_dir() {
+  local task_name="${tasks_not_done[$num_task]}"
+
+  normalized_task_name=$(sed -r '
+    y/àáâãäåèéêëìíîïòóôõöùúûü/aaaaaaeeeeiiiiooooouuuu/
+    y/ÀÁÂÃÄÅÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜ/AAAAAAEEEEIIIIOOOOOUUUU/
+    y/çÇñÑß¢Ðð£Øø§µÝý¥¹²³ªº/cCnNBcDdLOoSuYyY123ao/
+    y/ /_/
+    s_[:?"*<>|\/]__g
+    s_([^[:alnum:]])\1_\1_g' <<< "${task_name,,}")
+
+  file="$MISCELLANEOUS_DIR/${normalized_task_name}.$extension"
+  dir="$CURR_DIR/${normalized_task_name}"
+}
 
 
 list_not_done=$(grep -P -o '(?<=- \[ \] \[).+(?=\])' "$PATH_TO_FILE")
@@ -34,9 +53,10 @@ nums_tasks=$(grep -Pc '^- \[.\].+' "$PATH_TO_FILE")
 
 
 awk '{ print NR ":\t" $0 } END { print "\n" }' <<< "$list_not_done"
-read -a task_metadata -p "[task] done = "
+read -a task_metadata -p "[${COLORS[p]}task${COLORS[n]}] [${COLORS[p]}extension${COLORS[n]}] = "
 
 num_task=$((task_metadata[0]-1))
+extension=${task_metadata[1]}
 task_name=${tasks_not_done[num_task]}
 
 ## TODO: retornar para o 'read' se o número dado for inválido
@@ -45,16 +65,28 @@ task_name=${tasks_not_done[num_task]}
 line_selected_task=$(grep -n -m1 -F "$task_name" "$PATH_TO_FILE" | cut -d: -f1)
 
 if [[ -n ${task_metadata[0]} ]]; then
-  read -n1 -p "Task \"$task_name\" was done? (y/N) "
-  [[ "${REPLY,,}" == "y" ]] && sed -i "${line_selected_task}s/\[ \]/[x]/" "$PATH_TO_FILE"
+  ## TODO: retornar para o 'read' se a resposta for N
+  read -n1 -p "${COLORS[w]}Task ${COLORS[y]}${task_name}${COLORS[w]} was done? ${COLORS[r]}(y/N)${COLORS[n]} "
+
+  if [[ "${REPLY,,}" == "y" ]]; then
+    set_file_and_dir
+
+    sed -i "${line_selected_task} s/\[ \]/[x]/" "$PATH_TO_FILE"
+
+    [[ -n "$extension" && -e "$file" ]] && task_ref="./$MISCELLANEOUS/${normalized_task_name}.$extension"
+    [[ -d "$dir"  ]] && task_ref="./$normalized_task_name"
+
+    [[ -n "$task_ref" ]] && sed -ri "${line_selected_task} s%$% [:memo:](${task_ref})%" "$PATH_TO_FILE"
+  fi
 fi
 
 
 nums_tasks_done=$(grep -Pc '^- \[x\].+' "$PATH_TO_FILE")
 percentage=$(( nums_tasks_done*100/nums_tasks ))
+[[ percentage -eq 100 ]] && opt='g'
 
 sed -i -r "
   s/done-[0-9]+%/done-${percentage}%/
   t end
   :end
-" "$PATH_TO_FILE" && echo -e "\nNow: ${percentage}% done!"
+" "$PATH_TO_FILE" && echo -e "\n${COLORS[w]}Now ${COLORS[${opt:-r}]}${percentage}% ${COLORS[w]}done!${COLORS[n]}"
